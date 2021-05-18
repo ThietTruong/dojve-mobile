0c5160bbbf795ce63a427a89fc8e88016cbaecae
ADMIN\Admin <vycaoqer@gmail.com>
2021-05-18 19:10:28 +0700

    gui anh ui

--- src/components/ImageAndVideo/index.js
+++ src/components/ImageAndVideo/index.js
@@ -1,84 +1,16 @@
+import React, {useState} from 'react'
+import { View, Text, Image, Button, TouchableHighlight, TouchableOpacity } from 'react-native'
+import * as ImagePicker from "react-native-image-picker"
+import * as CameraRollPicker from 'react-native-camera-roll-picker';
+import axios from "../../utility/axios";
 
-// import React, {useState} from 'react'
-// import { View, Text, Image, Button, TouchableHighlight, TouchableOpacity } from 'react-native'
-// import * as ImagePicker from "react-native-image-picker"
-// import * as CameraRollPicker from 'react-native-camera-roll-picker';
-// import axios from "../../utility/axios";
+function ImageAndVideo ({sendAMessage}) {
+  const [media, setMedia] = useState({});
+  const sending = (media) => {
+    sendAMessage(2, media);
+  };
 
-// function ImageAndVideo ({sendAMessage}) {
-//   const [media, setMedia] = useState({});
-//   const sending = (media) => {
-//     sendAMessage(2, media);
-//   };
-
-//   const handleChoosePhoto = () => {
-//     const options = {
-//       noData: true,
-//     }
-//     ImagePicker.launchImageLibrary(options, response => {
-//       if (response.uri) {
-//         setMedia({ media: response })
-//       }
-//     })
-//   }
-//   const createFormData = (photo, body) => {
-//     const data = new FormData();
-//     data.append("photo", {
-//       name: photo.fileName,
-//       type: photo.type,
-//       uri:
-//         Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
-//     });
-//     Object.keys(body).forEach(key => {
-//       data.append(key, body[key]);
-//     });
-//     return data;
-//   };
-
-//   const handleUploadPhoto = () => {
-//     axios.post("http://localhost:5000/message/upImage", {
-//       method: "POST",
-//       body: createFormData(media, { userId: "123" })
-//     })
-//       .then(response => response.json())
-//       .then(response => {
-//         console.log("upload succes", response);
-//         alert("Upload success!");
-//         this.setState({ photo: null });
-//       })
-//       .catch(error => {
-//         console.log("upload error", error);
-//         alert("Upload failed!");
-//       });
-//   };
-
-
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' ,backgroundColor: 'white' }}>
-//     {media && (
-//         <TouchableHighlight onPress={sending}>
-//         <Image
-//           source={{ uri: media.uri }}
-//           style={{ width: 150, height: 150 }}
-//         />
-//         </TouchableHighlight>
-//     )}
-//     <Button title="Upload Photo" onPress={handleUploadPhoto} />
-//     <Button title="Choose Photo" onPress={handleChoosePhoto} />
-//   </View>
-//     )}
-
-// export default ImageAndVideo;
-
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Alert, Image } from 'react-native';
-import { STYLES, COLORS } from './styles';
-import ImagePicker from 'react-native-image-picker';
-
-export default function SimpleImagePicker() {
-  const [imageSource, setImageSource] = useState([]);
-
-  function selectImage() {
+  const handleChoosePhoto = () => {
     let options = {
       title: 'You can choose one image',
       maxWidth: 256,
@@ -100,42 +32,56 @@ export default function SimpleImagePicker() {
         console.log('User tapped custom button: ', response.customButton);
       } else {
         let source = { uri: response.uri };
-        setImageSource(source.uri);
+        setMedia(source.uri);
       }
     });
   }
 
+  const createFormData = (photo, body) => {
+    const data = new FormData();
+    data.append("photo", {
+      name: photo.fileName,
+      type: photo.type,
+      uri:
+        Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+    });
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+    return data;
+  };
+
+  // const handleUploadPhoto = () => {
+  //   axios.post("http://localhost:5000/message/upImage", {
+  //     method: "POST",
+  //     body: createFormData(media, { userId: "123" })
+  //   })
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       console.log("upload succes", response);
+  //       alert("Upload success!");
+  //       this.setState({ photo: null });
+  //     })
+  //     .catch(error => {
+  //       console.log("upload error", error);
+  //       alert("Upload failed!");
+  //     });
+  // };
+
+
   return (
-    <View
-      style={[
-        STYLES.flex,
-        STYLES.centerContainer,
-        { backgroundColor: COLORS.primaryDark }
-      ]}
-    >
-      <View style={STYLES.imageContainer}>
-        {imageSource === null ? (
-          <Image
-            style={STYLES.imageBox}
-            resizeMode='contain'
-          />
-        ) : (
-          <Image
-            source={{ uri: imageSource }}
-            style={STYLES.imageBox}
-            resizeMode='contain'
-          />
-        )}
-      </View>
-      <TouchableOpacity
-        onPress={selectImage}
-        style={[
-          STYLES.selectButtonContainer,
-          { backgroundColor: COLORS.primaryLight }
-        ]}
-      >
-        <Text style={STYLES.selectButtonTitle}>Pick an image</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' ,backgroundColor: 'white' }}>
+    {media && (
+        <TouchableHighlight>
+        <Image
+          source={{ uri: media.uri }}
+          style={{ width: 150, height: 150 }}
+        />
+        </TouchableHighlight>
+    )}
+    <Button title="Send Photo" onPress={sending} />
+    <Button title="Choose Photo" onPress={handleChoosePhoto} />
+  </View>
+    )}
+
+export default ImageAndVideo;
